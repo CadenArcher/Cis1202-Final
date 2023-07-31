c200be3ed01420cf558f262c467f7d90b2b381ff
CadenArcher <cadenarcher45@gmail.com>
2023-07-31 14:35:09 -0400

    Running a test to see how I could get a user to launch a url from the command line. So far I found this was the easist I found. And weighing either using a system function, or createprocess function the system function would be better. As I can use the system function on any computer not just windows, and I dont need the extra control the createprocess gives

--- Cis1202 Final/Cis1202 Final.cpp
+++ Cis1202 Final/Cis1202 Final.cpp
@@ -1,10 +1,18 @@
-
+#include <windows.h>
+#include <shellapi.h>
+#include <locale>
+#include <codecvt>
 #include <iostream>
+using namespace std;
 
 int main()
 {
-
-
-   
+    string Video;
+    cin >> Video;
+    string command = "start " + Video;
+    system(command.c_str());
+    cout << Video;
+    
+    return 0;
 }
 
